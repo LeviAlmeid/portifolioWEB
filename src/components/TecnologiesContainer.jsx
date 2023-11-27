0aae38e5779da3f9a981c0b6dc6cfcc332e8b5b4
LeviAlmeid <levialmeida02@outlook.com>
2023-11-27 11:38:30 -0300

    desc cards projects

--- src/components/TecnologiesContainer.jsx
+++ src/components/TecnologiesContainer.jsx
@@ -10,12 +10,12 @@ import{
 import '../styles/components/tecnologiescontainer.scss'
 
 const technologies = [
-    { id: 'html', name: 'HTML5', icon: <DiHtml5 />},
-    { id: 'css', name: 'CSS3', icon: <DiCss3 />},
-    { id: 'js', name: 'JavaScript', icon: <DiJsBadge />},
-    { id: 'node', name: 'Node.Js', icon: <DiNodejsSmall />},
-    { id: 'mysql', name: 'MySQL', icon: <DiMysql />},
-    { id: 'react', name: 'React', icon: <DiReact />},
+    { id: 'html', name: 'HTML5', icon: <DiHtml5 />, desc: "Explorar o HTML foi uma jornada simples e gratificante, tornando a criação de uma página mais intuitiva."},
+    { id: 'css', name: 'CSS3', icon: <DiCss3 />, desc: "Experimentar CSS foi como dar vida à minha página web, podendo deixar o visual de maneira incrivelmente flexível e intuitiva."},
+    { id: 'js', name: 'JavaScript', icon: <DiJsBadge />, desc: "Explorar JavaScript foi como desbloquear o interativo da minha página, proporcionando uma experiência diferente."},
+    { id: 'node', name: 'Node.Js', icon: <DiNodejsSmall />, desc: "Node.js abriu novas possibilidades, permitindo-me levar o JavaScript para o lado do servidor."},
+    { id: 'mysql', name: 'MySQL', icon: <DiMysql />, desc: "MySQL trouxe robustez aos meus projetos, proporcionando uma base de dados confiável e eficiente"},
+    { id: 'react', name: 'React', icon: <DiReact />, desc: "Explorar React foi como descobrir uma abordagem revolucionária para construir interfaces de usuário."},
 ]
 
 const TecnologiesContainaer = () => {
@@ -28,7 +28,7 @@ const TecnologiesContainaer = () => {
                         {tech.icon}
                         <div className="teclogy-info">
                             <h3>{tech.name}</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                            <p>{tech.desc}</p>
                         </div>
                     </div>
                 ))}
